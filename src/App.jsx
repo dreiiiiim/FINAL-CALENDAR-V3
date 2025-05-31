7e0673f78466309d5e175e02c8e6ce09f6d80670
dreiiiiim <andrei.montaniel.cics@ust.edu.ph>
2025-05-31 20:51:40 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,41 @@ const App = () => {
   }, [navigate]);
 
   return (
-    <div className="h-screen w-screen">
-      <Routes>
-        <Route path="/" element={<Welcomepage />} />
-        <Route path="/MonthlyCalendar" element={<MonthlyCalendar />} />
-      </Routes>
-      <AuthRedirect />
-    </div>
+   import React, { useEffect } from "react";
+import MonthlyCalendar from "./Components/MonthlyCalendar";
+import Welcomepage from "./Components/Welcomepage";
+import AuthRedirect from "./Components/AuthRedirect";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { supabase } from "./Components/client";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      if (data?.session) {
+        navigate("/MonthlyCalendar");
+      }
+    };
+    checkSession();
+  }, [navigate]);
+
+  return (
+  <Router>
+      <div className='h-screen w-screen'>
+        <Routes>
+          <Route path='/' element={<Welcomepage />} />
+          <Route path='/MonthlyCalendar' element={<MonthlyCalendar />} />
+          <Route path='*' element={<AuthRedirect />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
   );
 };
 
